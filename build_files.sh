61f8ac4f1760a210cb1dd90424b8c34b3265cbfa
Shubham Lohan <shubhamlohan99@gmail.com>
2025-12-24 03:22:51 +0530

    Use virtual environment in build script

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 # build_files.sh
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
